fbe5f193f3230aa22b9cf622796fba98a1b78098
Ken Tsang <kenlt.uk@gmail.com>
2018-10-12 18:44:31 +0100

    Add environment_sample.sh to make it easier for new contributors to join

---
+++ environment_sample.sh
@@ -0,0 +1,7 @@
+export SECRET_KEY=<your secret>
+export API_BASE_URL=<api url, can be local or remmote>
+export FRONTEND_BASE_URL=<frontend url>
+export ADMIN_CLIENT_ID=<must match api client id>
+export ADMIN_CLIENT_SECRET=<must match api client secret>
+export AUTH_USERNAME=<basic auth username for protected endpoints>
+export AUTH_PASSWORD=<basic auth password for protected endpoints>
