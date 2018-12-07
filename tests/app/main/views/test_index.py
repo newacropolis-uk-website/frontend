11225f687cd40ac03b919a0ccd758f391f6814b3
amritsiidhu <amritsidhu360@gmail.com>
2018-12-07 23:08:06 +0000

    Added tests to check correct contentd are being displayed

--- tests/app/main/views/test_index.py
+++ tests/app/main/views/test_index.py
@@ -7,6 +7,23 @@ from tests.conftest import AUTH_USERNAME, AUTH_PASSWORD
 
 
 class WhenAccessingHomePage(object):
+    def it_should__display_banner_text(self, client, mocker):
+        response = client.get(url_for(
+            'main.index'
+        ))
+        page = BeautifulSoup(response.data.decode('utf-8'), 'html.parser')
+        content = page.find("div", {"class": "banner_findoutmore"}).string
+        assert content == 'FIND OUT MORE'
+
+
+class WhenAccessingHomePage(object):
+    def it_should_display_header_content(self, client, mocker):
+        response = client.get(url_for(
+            'main.index'
+        ))
+        page = BeautifulSoup(response.data.decode('utf-8'), 'html.parser')
+        content = page.find("div", {"class": "header_about"}).string
+        assert content == 'About'
 
     def it_shows_list_of_available_pages(self, client, mocker):
         response = client.get(url_for(
