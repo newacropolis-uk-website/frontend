9634ab29b02308e20a86ca258369fe504b460253
Ken Tsang <kenlt.uk@gmail.com>
2019-01-01 23:54:18 +0000

    Add test for carousel

--- tests/app/main/views/test_index.py
+++ tests/app/main/views/test_index.py
@@ -21,7 +21,30 @@ class WhenAccessingHomePage(object):
         ))
         page = BeautifulSoup(response.data.decode('utf-8'), 'html.parser')
         content = page.find("div", {"class": "whatson_block_subheading"}).string
-        assert content == sample_future_events[0]['title']
+
+        intro_course = [e for e in sample_future_events if e['event_type'] == 'Introductory Course'][0]
+
+        assert content == intro_course['title']
+
+    def it_should_show_future_events_in_carousel(self, client, sample_future_events, sample_articles_summary):
+        response = client.get(url_for(
+            'main.index'
+        ))
+
+        other_events = []
+        for event in sample_future_events:
+            if event['event_type'] != 'Introductory Course':
+                other_events.append(event)
+
+        page = BeautifulSoup(response.data.decode('utf-8'), 'html.parser')
+        carousel_items = page.select(".carousel_info_text")
+        for i, event in enumerate(sample_future_events):
+            if event['event_type'] == 'Introductory Course':
+                # expect the first event in the carousel to be an intro course
+                assert carousel_items[0].text.strip() == "Courses starting January 2019"
+            else:
+                # expect the other events to be after an intro course
+                assert carousel_items[i + 1].text.strip() == other_events[i]['title']
 
     @pytest.mark.parametrize('div_class', ['.header', '.footer'])
     def it_shows_list_of_available_pages_on_header_and_footer(
