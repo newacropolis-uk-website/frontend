d63dc1dce223046e78f507ad3ed0d271f61c48bc
Ken Tsang <kenlt.uk@gmail.com>
2019-01-16 00:46:46 +0000

    Add tests for google auth on admin

---
+++ tests/app/main/views/test_admin.py
@@ -0,0 +1,58 @@
+import uuid
+from bs4 import BeautifulSoup
+from flask import json, url_for, request
+
+
+def mock_oauth2session(mocker, auth_url):
+
+    class MockOAuth2Session:
+        def __init__(*args, **kwargs):
+            pass
+
+        def authorization_url(self, url):
+            return auth_url, 'state'
+
+        def fetch_token(*args, **kwargs):
+            return 'token'
+
+        def get(self, url):
+
+            class MockProfile:
+                def json(self):
+                    return {
+                        'email': 'test@example.com'
+                    }
+
+            return MockProfile()
+
+    mocker.patch('app.main.views.OAuth2Session', MockOAuth2Session)
+
+
+class WhenAccessingAdminPagesWithoutLoggingIn(object):
+
+    def it_redirects_to_google_auth(self, client, mocker):
+        mock_oauth2session(mocker, 'http://auth_url')
+
+        mocker.patch('app.main.views.session', {})
+        mocker.patch('app.main.views.os.environ', {})
+
+        response = client.get(url_for(
+            'main.admin'
+        ))
+        assert response.status_code == 302
+        assert response.location == 'http://auth_url'
+
+    def it_stores_the_profile_in_session(self, client, mocker):
+        mock_oauth2session(mocker, url_for('main.callback'))
+
+        session_dict = {
+            'oauth_state': 'state'
+        }
+        mocker.patch('app.main.views.session', session_dict)
+        mocker.patch('app.main.views.os.environ', {})
+
+        response = client.get(url_for(
+            'main.callback'
+        ), follow_redirects=True)
+        assert response.status_code == 200
+        assert session_dict['user_profile'] == {'email': 'test@example.com'}
