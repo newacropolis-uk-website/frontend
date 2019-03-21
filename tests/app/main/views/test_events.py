fa7aa6ac369e0aa5551daa912721d9ee37407db2
Ken Tsang <kenlt.uk@gmail.com>
2019-03-21 01:39:40 +0000

    Add test for admin events

---
+++ tests/app/main/views/test_events.py
@@ -0,0 +1,147 @@
+from flask import url_for
+
+from bs4 import BeautifulSoup
+
+
+class MockAPIClient:
+    def get_limited_events(self):
+        return [
+            {
+                "event_dates": [
+                    {
+                        "event_id": "9ad571e1-4b5e-49af-a814-0958b23888c5",
+                        "speakers": [],
+                        "end_time": None,
+                        "event_datetime": "2018-01-20 19:00",
+                        "id": "fe8e3d17-bef4-48e9-b22b-971cee7276fa"
+                    }
+                ],
+                "fee": 5,
+                "event_type": "short course",
+                "old_id": 1,
+                "sub_title": None,
+                "title": "2018 event",
+                "image_filename": None,
+                "multi_day_conc_fee": 10,
+                "multi_day_fee": 12,
+                "venue": None,
+                "event_type_id": "7818e99a-8c54-40a3-a790-306d9694c4b9",
+                "conc_fee": 3,
+                "booking_code": None,
+                "id": "9ad571e1-4b5e-49af-a814-0958b23888c5",
+                "description": "test description"
+            },
+            {
+                "event_dates": [
+                    {
+                        "event_id": "c1af44b3-1bba-4d6d-996a-162072ebbe53",
+                        "speakers": [],
+                        "end_time": None,
+                        "event_datetime": "2018-01-01 19:00",
+                        "id": "e630d711-65f9-41d4-b305-f2e7769a08a8"
+                    }
+                ],
+                "fee": 5,
+                "event_type": "workshop",
+                "old_id": 1,
+                "sub_title": None,
+                "title": "test_title",
+                "image_filename": None,
+                "multi_day_conc_fee": 10,
+                "multi_day_fee": 12,
+                "venue": None,
+                "event_type_id": "1289f71f-5127-4b1f-a615-efa94b930a36",
+                "conc_fee": 3,
+                "booking_code": None,
+                "id": "c1af44b3-1bba-4d6d-996a-162072ebbe53",
+                "description": "test description"
+            }
+        ]
+
+    def get_event_types(self):
+        return [
+            {
+                "repeat": 1,
+                "event_type": "short course",
+                "old_id": 1,
+                "created_at": "Thu, 21 Mar 2019 00:24:55 GMT",
+                "id": "80285428-b2a1-4f6e-acfb-7e6f4f04bd0b",
+                "event_filename": None,
+                "fees": [],
+                "duration": 45,
+                "repeat_interval": 0,
+                "event_desc": "test talk"
+            }
+        ]
+
+    def get_speakers(self):
+        return [
+            {
+                "parent_id": None,
+                "title": "Mr",
+                "id": "cbbf7c1d-b12b-4c69-a06c-95b462a21268",
+                "name": "Paul White"
+            }
+        ]
+
+    def get_venues(self):
+        return [
+            {
+                "name": "Head office",
+                "old_id": 1,
+                "default": True,
+                "address": "10 London Street, N1 1NN",
+                "directions": "By bus: 100, 111, 123",
+                "id": "83f4e670-bb46-4282-a2a0-aead23de6c0d"
+            }
+        ]
+
+
+class WhenShowingEvents:
+    def it_populates_all_fields_in_admin_events(self, client, mocker, mock_admin_logged_in):
+        mocker.patch('app.main.views.admin.api_client', MockAPIClient())
+
+        response = client.get(url_for(
+            'main.admin_events'
+        ))
+
+        assert response.status_code == 200
+
+        page = BeautifulSoup(response.data.decode('utf-8'), 'html.parser')
+
+        select_events = page.select_one('#events')
+
+        event_options = select_events.select('option')
+
+        assert len(event_options) == 3
+
+        assert event_options[0].text == 'New event'
+        assert str(event_options[1]) == (
+            '<option value="9ad571e1-4b5e-49af-a814-0958b23888c5">2018-01-20'
+            ' 19:00 - short course - 2018 event</option>')
+        assert str(event_options[2]) == (
+            '<option value="c1af44b3-1bba-4d6d-996a-162072ebbe53">2018-01-01 19:00 - workshop - test_title</option>')
+
+        select_event_types = page.select_one('#event_type')
+
+        event_type_options = select_event_types.select('option')
+
+        assert str(event_type_options[0]) == (
+            '<option value="80285428-b2a1-4f6e-acfb-7e6f4f04bd0b">short course</option>')
+
+        select_venues = page.select_one('#venue')
+
+        venue_options = select_venues.select('option')
+
+        assert str(venue_options[0]) == (
+            '<option value="83f4e670-bb46-4282-a2a0-aead23de6c0d">Head office - 10 London Street, N1 1NN</option>')
+
+
+class WhenCallingAjaxDeleteEvent:
+    def it_makes_delete_api_call(self):
+        pass
+
+
+class WhenCallingAjaxGetEvent:
+    def it_makes_get_api_call(self):
+        pass
